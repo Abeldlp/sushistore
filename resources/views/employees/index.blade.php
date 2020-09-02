618839d5cced2aa9f762dcd011853707125e5c99
Abel de la Paz <abel45991690@gmail.com>
2020-09-02 03:53:10 +0200

    touched style for employees

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -4,7 +4,12 @@
     <h1 class="text-center">Employees</h1>
     <div class="container">
         @foreach ($users as $user)
-            <p>{{ $user->name }}</p>
+            <div class="d-flex m-2 p-2 align-items-center rounded w-80">
+                <div class="col-2 col-sm-2 col-md-1">
+                    <img class="mw-100 rounded-circle" src="/storage/images/noimage.png" alt="">
+                </div>
+                <span> {{ $user->name }} <strong>{{ $user->employee->position }}</strong> <a href="#">Edit</a></span>
+            </div>
         @endforeach
     </div>
 @endsection
